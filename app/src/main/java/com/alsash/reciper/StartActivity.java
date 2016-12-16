1281e67b46bcea2653376b0fb22faf07ea970ecb
alsash <alsashcom@gmail.com>
2016-12-16 12:00:46 +0200

    Add StartActivity in fullscreen mode

--- app/src/main/java/com/alsash/reciper/StartActivity.java
+++ app/src/main/java/com/alsash/reciper/StartActivity.java
@@ -28,6 +28,7 @@ public class StartActivity extends AppCompatActivity {
         }
     };
 
+    @SuppressLint("InlinedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,11 +38,17 @@ public class StartActivity extends AppCompatActivity {
             actionBar.hide();
         }
         contentView = findViewById(R.id.fullscreen_content);
+        contentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        hideHandler.postDelayed(hideRunnable, UI_ANIMATION_DELAY);
+        // hideHandler.postDelayed(hideRunnable, UI_ANIMATION_DELAY);
     }
 }
