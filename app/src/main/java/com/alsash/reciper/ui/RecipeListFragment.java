1805168f165393b156ccd8084c30b00a35ccc60d
alsash <alsashcom@gmail.com>
2017-01-17 20:04:23 +0200

    Add recipe list menu items

--- app/src/main/java/com/alsash/reciper/ui/RecipeListFragment.java
+++ app/src/main/java/com/alsash/reciper/ui/RecipeListFragment.java
@@ -3,6 +3,8 @@ package com.alsash.reciper.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -25,6 +27,9 @@ public class RecipeListFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setTitle("");
     }
 
     @Override
