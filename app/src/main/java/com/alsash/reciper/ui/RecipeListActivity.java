8aa9602e0db03661bac918b1fe73db841863039e
alsash <alsashcom@gmail.com>
2017-01-20 21:58:34 +0200

    Add recipe card

--- app/src/main/java/com/alsash/reciper/ui/RecipeListActivity.java
+++ app/src/main/java/com/alsash/reciper/ui/RecipeListActivity.java
@@ -6,33 +6,11 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 
-import com.alsash.reciper.R;
-
-public class RecipeListActivity extends DrawerSpinnerActivity {
+public class RecipeListActivity extends DrawerFrameActivity {
 
     @Override
-    @Nullable
-    protected Fragment getSpinnerFragment(int menuItemId, @Nullable Intent activityIntent) {
-        switch (menuItemId) {
-            case R.id.group_all:
-            case R.id.group_bookmark:
-            case R.id.group_label:
-            case R.id.group_category:
-            case R.id.group_food:
-                return RecipeListFragment.newInstance();
-            default:
-                return null;
-        }
-    }
-
-    @Override
-    protected int getSpinnerMenuRes() {
-        return R.menu.recipe_group;
-    }
-
-    @Override
-    protected int getSpinnerMenuItemDefault() {
-        return R.id.group_all;
+    protected Fragment getFrameFragment(@Nullable Intent activityIntent) {
+        return RecipeListFragment.newInstance();
     }
 
     @Override
