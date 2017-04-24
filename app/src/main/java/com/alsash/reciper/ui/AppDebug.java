6148f3f79ca2e9807091b5d95d98211c5bcc190d
alsash <alsashcom@gmail.com>
2017-04-24 22:10:04 +0300

    Remove Mosby library

--- app/src/main/java/com/alsash/reciper/ui/AppDebug.java
+++ app/src/main/java/com/alsash/reciper/ui/AppDebug.java
@@ -2,16 +2,18 @@ package com.alsash.reciper.ui;
 
 import android.app.Application;
 
+import com.alsash.reciper.BuildConfig;
 import com.facebook.stetho.Stetho;
 
 /**
  * Debugging tools. Don't forget remove this class from manifest on develops finish
  */
-
 public class AppDebug extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Stetho.initializeWithDefaults(this);
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this);
+        }
     }
 }
