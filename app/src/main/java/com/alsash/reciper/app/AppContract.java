71bf0be997a31e40844074d1d0df4a1c9887539c
alsash <alsashcom@gmail.com>
2017-05-13 22:36:02 +0300

    Create pagination with RxJava2

--- app/src/main/java/com/alsash/reciper/app/AppContract.java
+++ app/src/main/java/com/alsash/reciper/app/AppContract.java
@@ -9,5 +9,6 @@ public class AppContract {
     private static final String TAG = AppContract.class.getCanonicalName();
     public static final String PAYLOAD_FLIP_BACK_TO_FRONT = TAG + ".payload_flip_back_to_front";
     public static final String PAYLOAD_FLIP_FRONT_TO_BACK = TAG + ".payload_flip_front_to_back";
+
     public static final String KEY_RECIPE_ID = TAG + ".key_recipe_id";
 }
